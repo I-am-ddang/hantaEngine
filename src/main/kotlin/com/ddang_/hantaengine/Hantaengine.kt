4831beda90eb8e3b2b740f4dce35e76f3e16a0c4
DDang_ <psychologist3552@gmail.com>
2022-08-16 22:02:38 +0900

    Alpha-0.0.1 // basic

---
+++ src/main/kotlin/com/ddang_/hantaengine/Hantaengine.kt
@@ -0,0 +1,13 @@
+package com.ddang_.hantaengine
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Hantaengine : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
