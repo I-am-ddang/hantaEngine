5c81a7000881efa8b83f81db3102b3683d12ef21
DDang_ <psychologist3552@gmail.com>
2022-08-16 22:39:43 +0900

    Alpha-0.0.5 // InventoryName 클래스, GUIManager 클래스 추가

---
+++ src/main/kotlin/com/ddang_/hantaengine/enums/InventoryName.kt
@@ -0,0 +1,7 @@
+package com.ddang_.hantaengine.enums
+
+//커스텀 GUI 를 사전에 특정하기 위한 enum 클래스입니다.
+
+enum class InventoryName {
+
+}
---
+++ src/main/kotlin/com/ddang_/hantaengine/managers/GUIManager.kt
@@ -0,0 +1,19 @@
+package com.ddang_.hantaengine.managers
+
+import com.ddang_.hantaengine.enums.InventoryName
+import org.bukkit.inventory.Inventory
+
+//커스텀 GUI 를 구성하기 위한 매니저 클래스입니다.
+
+class GUIManager {
+    companion object {
+
+        //InventoryName 을 구분해 커스텀 GUI 를 반환하는 메소드입니다.
+
+        fun getInventory(name: InventoryName): Inventory? {
+            when (name) {
+                else -> return null
+            }
+        }
+    }
+}
